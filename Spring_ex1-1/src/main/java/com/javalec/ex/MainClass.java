d0263647dbd681a6da94fd74a250fb1323b3657f
Unknown <rhep0820@gmail.com>
2018-06-21 14:08:44 +0900

    Spring 적용안된 경우

---
+++ Spring_ex1-1/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,19 @@
+package com.javalec.ex;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		MyCalculator myCalculator = new MyCalculator();
+		myCalculator.setCalculator(new Calculator());
+		
+		myCalculator.setFirstNum(10);
+		myCalculator.setSecondNum(2);
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+	}
+
+}
---
+++ Spring_ex1-1/src/main/java/com/javalec/ex/MyCalculator.java
@@ -0,0 +1,44 @@
+package com.javalec.ex;
+
+public class MyCalculator {
+
+	Calculator calculator;
+	private int firstNum;
+	private int secondNum;
+	
+	public MyCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void add() {
+		calculator.addition(firstNum, secondNum);
+	}
+	
+	public void sub() {
+		calculator.subtraction(firstNum, secondNum);
+	}
+	
+	public void mul() {
+		calculator.multiplication(firstNum, secondNum);
+	}
+	
+	public void div() {
+		calculator.division(firstNum, secondNum);
+	}
+	
+	
+	
+	public void setCalculator(Calculator calculator) {
+		this.calculator = calculator;
+	}
+	
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+	
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+	
+}
