54706be4ee932acd87128a613f8e1a2f0230b9fa
Unknown <rhep0820@gmail.com>
2018-06-22 11:55:32 +0900

    @Aspect 로 AOP 구현하기

---
+++ Spring_ex4/src/main/java/com/javalec/ex/LogAop.java
@@ -0,0 +1,37 @@
+package com.javalec.ex;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class LogAop {
+
+	@Pointcut("within(com.javalec.ex.*")
+	private void pointcutMethod() {
+	}
+	
+	@Around("pointcutMethod()")
+	public Object loggerAop(ProceedingJoinPoint joinpoint) throws Throwable{
+		String signatureStr = joinpoint.getSignature().toShortString();
+		System.out.println(signatureStr + "is start.");
+		long st = System.currentTimeMillis();
+		
+		try {
+			Object obj = joinpoint.proceed();
+			return obj;
+		} finally {
+			long et = System.currentTimeMillis();
+			System.out.println(signatureStr + " is finished.");
+			System.out.println(signatureStr + "경과시간 : " + (et - st));
+		}
+	}
+	
+	@Before("within(com.javalec.ex.*)")
+	public void beforeAdvice() {
+		System.out.println("beforeAdvice()");
+	}
+	
+}
---
+++ Spring_ex4/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,21 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+		Student student = ctx.getBean("student", Student.class);
+		student.getStudentInfo();
+		
+		Worker worker = ctx.getBean("worker", Worker.class);
+		worker.getWorkerInfo();
+		
+		ctx.close();
+
+	}
+
+}
